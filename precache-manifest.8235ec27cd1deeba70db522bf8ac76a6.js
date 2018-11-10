eaca49ae0bc4f929e1e0fdd0c23b3400306c734d
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-10 13:41:48 -0600

    primer commit

---
+++ precache-manifest.8235ec27cd1deeba70db522bf8ac76a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d52b123962a2c954d9c5",
+    "url": "/static/js/main.d52b1239.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "d52b123962a2c954d9c5",
+    "url": "/static/css/main.e330c11d.chunk.css"
+  },
+  {
+    "revision": "361cb3b6384c541eec8bd7911ffd13d7",
+    "url": "/index.html"
+  }
+];
